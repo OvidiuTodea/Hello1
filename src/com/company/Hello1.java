0878605fb339264882d7249f96a426cfb0c71679
ovidiu todea <ovi.todea@yahoo.com>
2019-12-02 19:11:35 +0200

    Modificare comentarii mod de apel din CMD

--- src/com/company/Hello1.java
+++ src/com/company/Hello1.java
@@ -7,9 +7,11 @@ public class Hello1 {
         // java -version  -->vedem versiunea de java folosita in cmd
         /*
          in CMD :
+         (https://javarevisited.blogspot.com/2015/04/error-could-not-find-or-load-main-class-helloworld-java.html)
          1. set Path=C:\Program Files\Java\jdk1.8.0_231\bin;
-         2. javac filename.java  -->compile ;
-         3. java filename  -->run
+         2. D:\ITeaHome\JAVA\Hello1\src\com\company>javac Hello1.java
+         3. D:\ITeaHome\JAVA\Hello1\src>java com.company.Hello1
+
         */
         System.out.println("Hello, Cluj :) !");
         System.out.println("La multi ani, Romania! ");
